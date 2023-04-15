78b6fecc4a991dc09075356dfba1c4ea4efa264d
Kuira Edwards <61628854+kuiraedwards@users.noreply.github.com>
2023-04-14 20:25:40 -0400

    Add files via upload

--- BENSCHILLIBOWL/BENSCHILLIBOWL.c
+++ BENSCHILLIBOWL/BENSCHILLIBOWL.c
@@ -1,11 +1,10 @@
-
-// Kuira Edwards @02942519
-
 #include "BENSCHILLIBOWL.h"
-
 #include <assert.h>
 #include <stdlib.h>
 #include <time.h>
+
+//Kuira Edwards @02942519
+//Worked with Jamarri White and Keerthana Pullela
 
 bool IsEmpty(BENSCHILLIBOWL* bcb);
 bool IsFull(BENSCHILLIBOWL* bcb);
@@ -33,28 +32,28 @@ MenuItem PickRandomMenuItem() {
 /* Allocate memory for the Restaurant, then create the mutex and condition variables needed to instantiate the Restaurant */
 
 BENSCHILLIBOWL* OpenRestaurant(int max_size, int expected_num_orders) {
-  BENSCHILLIBOWL *bcb = (BENSCHILLIBOWL*) malloc(sizeof(BENSCHILLIBOWL));
-  bcb->orders = NULL;
-  bcb->current_size = 0;
-  bcb->max_size = max_size;
-  bcb->next_order = 1;
-  bcb->handled = 0;
-  bcb->expected = expected;
+    BENSCHILLIBOWL *bcb = (BENSCHILLIBOWL*) malloc(sizeof(BENSCHILLIBOWL));
+    bcb->orders = NULL;
+    bcb->current_size = 0;
+    bcb->max_size = max_size;
+    bcb->next_order_number = 1;
+    bcb->orders_handled = 0;
+    bcb->expected_num_orders = expected_num_orders;
 
-  pthread_mutex_init(&(bcb->mutex), NULL);
-  pthread_cond_init(&(bcb->add_orders), NULL);
-  pthread_cond_init(&(bcb->get_orders), NULL);
-
-  printf("Restaurant is open!\n");
-  return bcb;
+    //Create the mutex and condition variables needed
+    pthread_mutex_init(&(bcb->mutex), NULL);
+    pthread_cond_init(&(bcb->can_add_orders), NULL);
+    pthread_cond_init(&(bcb->can_get_orders), NULL);
+    printf("Restaurant is open!\n");
+    return bcb;
 }
 
 
 /* check that the number of orders received is equal to the number handled (ie.fullfilled). Remember to deallocate your resources */
 
 void CloseRestaurant(BENSCHILLIBOWL* bcb) {
-  if(bcb->handled != bcb->expected) {
-    fprintf(stderr, "Orders not handles!\n");
+  if (bcb->orders_handled != bcb->expected_num_orders) {
+    printf(stderr, "Some of the orders were not handled.\n");
     exit(0);
   }
   pthread_mutex_destroy(&(bcb->mutex));
@@ -65,14 +64,14 @@ void CloseRestaurant(BENSCHILLIBOWL* bcb) {
 /* add an order to the back of queue */
 int AddOrder(BENSCHILLIBOWL* bcb, Order* order) {
   pthread_mutex_lock(&(bcb->mutex));
-  while(IsFull(bcb)) {
-    pthread_cond_wait(&(bcb->add_orders), &(bcb->mutex));
+  while (IsFull(bcb)) {
+    pthread_cond_wait(&(bcb->can_add_orders), &(bcb->mutex));
   }
-  order->order_number = bcb->next_order;
+  order->order_number = bcb->next_order_number;
   AddOrderToBack(&(bcb->orders), order);
-  bcb->next_order += 1;
+  bcb->next_order_number += 1;
   bcb->current_size += 1;
-  pthread_cond_broadcast(&(bcb->get_orders));
+  pthread_cond_broadcast(&(bcb->can_get_orders));
   pthread_mutex_unlock(&(bcb->mutex));
   return order->order_number;
 }
@@ -80,54 +79,52 @@ int AddOrder(BENSCHILLIBOWL* bcb, Order* order) {
 /* remove an order from the queue */
 Order *GetOrder(BENSCHILLIBOWL* bcb) {
   pthread_mutex_lock(&(bcb->mutex));
-  while(IsEmpty(bcb)) {
-    if(bcb->handled >= bcb->expected) {
+  while (IsEmpty(bcb)) {
+    if (bcb->orders_handled >= bcb->expected_num_orders) {
       pthread_mutex_unlock(&(bcb->mutex));
       return NULL;
     }
-    pthread_cond_wait(&(bcb->get_orders), &(bcb->mutex));
+    pthread_cond_wait(&(bcb->can_get_orders), &(bcb->mutex));
   }
   Order *order = bcb->orders;
   bcb->orders = bcb->orders->next;
 
   bcb->current_size -= 1;
-  bcb->handled += 1;
-
-  pthread_cond_broadcast(&(bcb->add_orders));
-
+  bcb->orders_handled += 1;
+  pthread_cond_broadcast(&(bcb->can_add_orders));
   pthread_mutex_unlock(&(bcb->mutex));
   return order;
 }
 
 // Optional helper functions (you can implement if you think they would be useful)
 bool IsEmpty(BENSCHILLIBOWL* bcb) {
-  if(bcb->current_size == 0) {
-    return true;
-  }
+  if (bcb->current_size == 0) {
+  return true;
+}
   else {
-    return false;
-  }
+  return false;
+}
 }
 
 bool IsFull(BENSCHILLIBOWL* bcb) {
-  if(bcb->current_size == bcb->max_size) {
+  if (bcb->current_size == bcb->max_size){
     return true;
   }
-  else {
+  else{
     return false;
   }
 }
 
 /* this methods adds order to rear of queue */
 void AddOrderToBack(Order **orders, Order *order) {
-  if(*orders == NULL) {
-    *orders = order;
-  }
+  if (*orders == NULL) {
+      *orders = order;
+  } 
   else {
-    Order *current = *orders;
-    while (current->next) {
-      current = current->next;
+      Order *curr_order = *orders;
+      while (curr_order->next) {
+          curr_order = curr_order->next;
+      }
+      curr_order->next = order;
     }
-    current->next = order;
-  }
 }
--- BENSCHILLIBOWL/main.c
+++ BENSCHILLIBOWL/main.c
@@ -6,10 +6,13 @@
 
 // Feel free to play with these numbers! This is a great way to
 // test your implementation.
-#define BENSCHILLIBOWL_SIZE 100
-#define NUM_CUSTOMERS 90
-#define NUM_COOKS 10
-#define ORDERS_PER_CUSTOMER 3
+
+// Received help from Keerthana Pullela and Jamarri White
+// Kuira Edwards @02942519
+#define BENSCHILLIBOWL_SIZE 10
+#define NUM_CUSTOMERS 10
+#define NUM_COOKS 2
+#define ORDERS_PER_CUSTOMER 1
 #define EXPECTED_NUM_ORDERS NUM_CUSTOMERS * ORDERS_PER_CUSTOMER
 
 // Global variable for the restaurant.
@@ -24,6 +27,15 @@ BENSCHILLIBOWL *bcb;
  */
 void* BENSCHILLIBOWLCustomer(void* tid) {
     int customer_id = (int)(long) tid;
+    for (int i = 0; i < ORDERS_PER_CUSTOMER; i++) {
+      Order *order = (Order*) malloc(sizeof(Order));
+      MenuItem menu_item = PickRandomMenuItem();
+      order->customer_id = customer_id;
+      order->menu_item = menu_item;
+      order->next = NULL;
+      int order_number = AddOrder(bcb, order);
+      printf("Customer #%d added order #%d\n", customer_id, order_number);
+    }
 	return NULL;
 }
 
@@ -36,8 +48,16 @@ void* BENSCHILLIBOWLCustomer(void* tid) {
  * receive an order.
  */
 void* BENSCHILLIBOWLCook(void* tid) {
-    int cook_id = (int)(long) tid;
+  int cook_id = (int)(long) tid;
 	int orders_fulfilled = 0;
+  Order *order;
+  order = GetOrder(bcb);
+
+  while (order != NULL) {
+    free(order);
+    orders_fulfilled += 1;
+    order = GetOrder(bcb);
+  }
 	printf("Cook #%d fulfilled %d orders\n", cook_id, orders_fulfilled);
 	return NULL;
 }
@@ -50,5 +70,28 @@ void* BENSCHILLIBOWLCook(void* tid) {
  *  - close the restaurant.
  */
 int main() {
-    return 0;
+  bcb = OpenRestaurant(BENSCHILLIBOWL_SIZE, EXPECTED_NUM_ORDERS);
+  pthread_t customers[NUM_CUSTOMERS];
+  pthread_t cooks[NUM_COOKS];
+  int customer_ids[NUM_CUSTOMERS];
+  int cook_ids[NUM_COOKS];
+  int i;
+  
+  for(i =0;i < NUM_COOKS; i++){
+    cook_ids[i]=i+1;
+    pthread_create(&cooks[i], NULL, &BENSCHILLIBOWLCook, (void *) &(cook_ids[i]));
+  }
+  
+  for(i =0;i < NUM_CUSTOMERS; i++){
+    customer_ids[i]=i+1;
+    pthread_create(&customers[i], NULL, &BENSCHILLIBOWLCustomer, (void *) &(customer_ids[i]));
+  }
+                   
+  for(i =0;i < NUM_CUSTOMERS; i++){
+   pthread_join(customers[i], NULL);
+  }
+  for(i =0;i < NUM_COOKS; i++){
+   pthread_join(cooks[i], NULL);
+  }
+  CloseRestaurant(bcb);
 }
